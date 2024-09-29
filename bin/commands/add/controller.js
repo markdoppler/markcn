7c7fbee4500c4bb1b32234b79f21418525377b1a
markdoppler <himarkdoppler@gmail.com>
2024-09-29 10:55:27 -0300

    chore: Refactor add controller and improve code organization

--- bin/commands/add/controller.js
+++ bin/commands/add/controller.js
@@ -1,13 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
 import inquirer from 'inquirer';
-import { logMessage } from '../../libs/chalk.js'; // Ajusta la ruta según sea necesario
-import { __dirname, getModuleType, getRootPath, fileExists, addImport, capitalize } from '../../util/util.js'; // Reutiliza fileExists de util.js
+import { logMessage } from '../../libs/chalk.js'; // Adjust the path as necessary
+import { __dirname, getModuleType, getRootPath, fileExists, addImport, capitalize } from '../../util/util.js'; // Reuse fileExists from util.js
 
 const addController = async () => {
 
   try {
-    // Step 1: Solicitar el nombre del controller al usuario
+    // Step 1: Ask the user for the controller name
     const answers = await inquirer.prompt([
       {
         type: 'input',
@@ -25,22 +25,22 @@ const addController = async () => {
     const controllerName = answers.controllerName.trim();
     const moduleType = await getModuleType();
 
-    // Step 2: Definir el nombre del archivo y la ruta
+    // Step 2: Define the file name and path
     const fileName = `${controllerName}.controller.js`;
     const rootPath = await getRootPath();
     const filePath = path.join(rootPath, 'src', 'controllers', fileName);
 
-    // Step 3: Verificar si el archivo ya existe utilizando fileExists
+    // Step 3: Check if the file already exists using fileExists
     const fileAlreadyExists = await fileExists(filePath);
     if (fileAlreadyExists) {
       logMessage(`Error: Controller file already exists at ${filePath}. Operation aborted.`, 'red');
-      return; // Detener ejecución si el archivo ya existe
+      return; // Stop execution if the file already exists
     }
 
-    // Step 4: Crear la carpeta /src/controllers si no existe
+    // Step 4: Create the /src/controllers folder if it doesn't exist
     await fs.mkdir(path.dirname(filePath), { recursive: true });
 
-    // Step 5: Preparar el contenido de la plantilla
+    // Step 5: Prepare the template content
 
     const esmTemplateContent = `import { app } from "../libs/express.js";
 
@@ -56,11 +56,11 @@ app.get('/${controllerName}', (req, res) => {
 });
 `;
 
-    // Step 6: Escribir el contenido de la plantilla en el archivo del controller
+    // Step 6: Write the template content to the controller file
     await fs.writeFile(filePath, moduleType === 'ESM' ? esmTemplateContent : commonJsTemplateContent);
     logMessage(`Controller file created at ${filePath}`, 'green');
 
-    // Step 7: Importar el nuevo controller en app.js
+    // Step 7: Import the new controller in app.js
     const importPath = `./src/controllers/${fileName}`;
     await addImport(importPath);
     logMessage(`Controller imported in app.js`, 'green');
